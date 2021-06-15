c90072d4b89047efa6ffb5513dc1e8225a8ea1c1
Andrew Oseghale <aoseghale@lobeksoftware.com>
2021-06-14 23:30:39 -0400

    Episode 27 - Category Filters

--- app/Http/Livewire/StatusFilters.php
+++ app/Http/Livewire/StatusFilters.php
@@ -2,41 +2,37 @@
 
 namespace App\Http\Livewire;
 
-use App\Models\Idea;
 use App\Models\Status;
 use Livewire\Component;
 use Illuminate\Support\Facades\Route;
 
 class StatusFilters extends Component
 {
-    public $status = 'All';
+    public $status;
     public $statusCount;
-
-    protected $queryString = [
-        'status',
-    ];
 
     public function mount()
     {
         $this->statusCount = Status::getCount();
+        $this->status = request()->status ?? 'All';
 
         if (Route::currentRouteName() === 'idea.show') {
             $this->status = null;
-            $this->queryString = [];
         }
     }
 
     public function setStatus($newStatus)
     {
         $this->status = $newStatus;
+        $this->emit('queryStringUpdatedStatus', $this->status);
 
         // dd(Route::currentRouteName()); // returns livewire.message
 
-        // if ($this->getPreviousRouteName() === 'idea.show') {
-        return redirect()->route('idea.index', [
-            'status' => $this->status,
-        ]);
-        // }
+        if ($this->getPreviousRouteName() === 'idea.show') {
+            return redirect()->route('idea.index', [
+                'status' => $this->status,
+            ]);
+        }
     }
 
     public function render()
