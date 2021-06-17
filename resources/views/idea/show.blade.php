5c007f339da9122b89ecf941b040ae9f278f91af
Andrew Oseghale <aoseghale@lobeksoftware.com>
2021-06-17 04:06:53 -0400

    Episode 37 - Edit Idea CSS for Modal

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -15,6 +15,8 @@
         :votesCount="$votesCount"
     />
 
+    <livewire:edit-idea />
+
     <div class="comments-container relative space-y-6 md:ml-22 pt-4 my-8 mt-1">
 
         <div class="comment-container relative bg-white rounded-xl flex mt-4">
@@ -41,11 +43,13 @@
                         <div 
                             x-data="{ isOpen: false }"
                             class="flex items-center space-x-2">
-                            <button
-                                @click="isOpen = !isOpen" 
-                                class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3"
-                            >
-                                <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></path></svg>
+                            <div class="relative">
+                                <button
+                                    @click="isOpen = !isOpen" 
+                                    class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3"
+                                >
+                                    <svg fill="currentColor" width="24" height="6"><path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)"></path></svg>
+                                </button>
                                 <ul 
                                     x-cloak
                                     x-show.transition.origin.top.left="isOpen"
@@ -56,8 +60,8 @@
                                 >
                                     <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as Spam</a></li>
                                     <li><a href="#" class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete Post</a></li>
-                                </ul>
-                            </button>
+                                </ul>                                
+                            </div>
                         </div>
                     </div>
                 </div>
