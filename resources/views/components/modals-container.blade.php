31a241fce286dc9b93414e00ef30b52558799717
Andrew Oseghale <aoseghale@lobeksoftware.com>
2021-06-18 17:34:06 -0400

    Episode 45 - Refactor Modals to Blade Component.

---
+++ resources/views/components/modals-container.blade.php
@@ -0,0 +1,26 @@
+
+    @can('update', $idea)
+    <livewire:edit-idea
+        :idea="$idea"
+    />
+    @endcan
+
+    @can('delete', $idea)
+    <livewire:delete-idea
+        :idea="$idea"
+    />
+    @endcan
+
+    @auth
+    <livewire:mark-idea-as-spam
+        :idea="$idea"
+    />
+    @endauth
+
+    @admin
+        @if ($idea->spam_reports > 0)
+    <livewire:mark-idea-as-not-spam
+        :idea="$idea"
+    />
+        @endif
+    @endadmin
